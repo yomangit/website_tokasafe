cf879eed2aecacb02302ecc04f4dc607747a472b
yomangit <106853745+yomangit@users.noreply.github.com>
2025-01-24 23:15:58 +0800

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,7 +7,6 @@ import "flatpickr/dist/flatpickr.min.css";
 import monthSelectPlugin from "flatpickr/dist/plugins/monthSelect";
 import "flatpickr/dist/themes/dark.css";
 import "flatpickr/dist/plugins/monthSelect/style.css";
-
 if ('serviceWorker' in navigator) 
 {
     navigator.serviceWorker.register('/sw.js', { scope: '/' }).then(function (registration) {
