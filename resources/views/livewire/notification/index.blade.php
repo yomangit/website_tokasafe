18b17f387d3efb617e2400c2c199de93aefb8cec
yomangit <106853745+yomangit@users.noreply.github.com>
2025-02-03 16:23:12 +0800

    Update index.blade.php

--- resources/views/livewire/notification/index.blade.php
+++ resources/views/livewire/notification/index.blade.php
@@ -20,7 +20,7 @@
                         <table class="table table-zebra table-xs">
                             <tbody>
                                 @foreach ($AllNotification as $notification)
-                                @dd(json_encode($notification->data["line"]))
+                                @dd(json_encode($notification->data[0]))
                                     <tr>
                                         <td>
                                             <label wire:click="goTo('{{ $notification->data[0] }}')"
