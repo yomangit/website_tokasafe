f4c4f60a1f3f951c71819a2c6b15a7c2d742a1b1
yomangit <106853745+yomangit@users.noreply.github.com>
2025-02-05 13:54:04 +0800

    fix: improve code formatting and indentation in notification index view

--- resources/views/livewire/notification/index.blade.php
+++ resources/views/livewire/notification/index.blade.php
@@ -11,19 +11,23 @@
                     class="text-[9px] text-white badge badge-xs bg-rose-500 indicator-item">{{ auth()->user()->unreadNotifications->count() <= 99 ? auth()->user()->unreadNotifications->count() : '99+' }}</span>
             </div>
         </label>
-        <div tabindex="0" class=" z-[1] card card-compact dropdown-content w-56 lg:w-96 h-56 lg:h-96 xl:h-[48rem] bg-base-100 shadow">
-            <div wire:poll class="card-body">
+        <div tabindex="0"
+            class=" z-[1] card card-compact dropdown-content w-56 lg:w-96 h-56 lg:h-96 xl:h-[48rem] bg-base-100 shadow">
+            <div wire:poll
+                class="card-body min-h-3/6 w-8/12 sm:w-8/12 sm:min-h-3/6 md:w-7/12 md:min-h-2.5/6 lg:w-5/12 lg:min-h-2.5/6 xl:w-4/12 xl:min-h-2.5/6 2xl:w-3.5/12 2xl:min-h-4/6">
 
-                <div  role="tablist" class="tabs tabs-bordered">
-                    <input type="radio" name="my_tabs_1" role="tab" class="tab " aria-label="All"  checked="checked"/>
+                <div role="tablist" class="tabs tabs-bordered">
+                    <input type="radio" name="my_tabs_1" role="tab" class="tab " aria-label="All"
+                        checked="checked" />
                     <div role="tabpanel" class="p-10 tab-content">
                         <table class="table table-zebra table-xs">
                             <tbody>
                                 @foreach ($AllNotification as $notification)
                                     <tr>
                                         <td>
-                                            <label wire:click="goTo('{{ $notification->id }}','{{ $notification->data['url'] }}')"
-                                                class="text-xs cursor-pointer font-signika {{ $notification->read_at == null? 'text-sky-500':'' }} ">
+                                            <label
+                                                wire:click="goTo('{{ $notification->id }}','{{ $notification->data['url'] }}')"
+                                                class="text-xs cursor-pointer font-signika {{ $notification->read_at == null ? 'text-sky-500' : '' }} ">
                                                 {{ $notification->data['line'] }}</label>
                                         </td>
                                     </tr>
@@ -31,7 +35,8 @@
                             </tbody>
                         </table>
                     </div>
-                    <input type="radio" name="my_tabs_1" role="tab" class="tab text-sky-500" aria-label="Unread"/>
+                    <input type="radio" name="my_tabs_1" role="tab" class="tab text-sky-500"
+                        aria-label="Unread" />
                     <div role="tabpanel" class="p-10 tab-content">
                         <table class="table table-zebra table-xs">
                             <tbody>
@@ -44,7 +49,7 @@
                                                 {{ $notification->data['line'] }}</label>
                                         </td>
                                     </tr>
-                                    @empty
+                                @empty
                                     <tr>
                                         <td>all notifications are read</td>
                                     </tr>
