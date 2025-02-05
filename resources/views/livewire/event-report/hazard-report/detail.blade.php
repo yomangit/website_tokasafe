f00ab59349e1ec85cd62b9397363ac2bb3695038
yomangit <106853745+yomangit@users.noreply.github.com>
2025-02-05 08:22:38 +0800

    fix: remove license key from CKEditor initialization for improved security

--- resources/views/livewire/event-report/hazard-report/detail.blade.php
+++ resources/views/livewire/event-report/hazard-report/detail.blade.php
@@ -503,7 +503,7 @@
 
                 ClassicEditor
                     .create(document.querySelector('#editor'), {
-                        licenseKey: '<YOUR_LICENSE_KEY>',
+
                         plugins: [Essentials, Bold, Italic, Font, Paragraph, FormatPainter],
                         toolbar: [
                             'undo', 'redo', '|', 'bold', 'italic', '|',
