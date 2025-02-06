53bd84288320c94a35fdfd3a23865d993355ec80
yomangit <106853745+yomangit@users.noreply.github.com>
2025-02-06 11:47:08 +0800

    fix: adjust query logic for EventUserSecurity in HazardReport component

--- app/Livewire/EventReport/HazardReport/Panal/Index.php
+++ app/Livewire/EventReport/HazardReport/Panal/Index.php
@@ -87,7 +87,7 @@ class Index extends Component
             dd($ERM);
             foreach ($ERM as $value) {
                 if (!empty($value)) {
-                    $this->EventUserSecurity = ( EventUserSecurity::whereIn('dept_by_business_unit_id', [$value])->where('type_event_report_id', $this->event_type_id)->exists())? EventUserSecurity::whereIn('dept_by_business_unit_id', [$value])->where('type_event_report_id', $this->event_type_id)->get(): EventUserSecurity::whereIn('dept_by_business_unit_id', [$value])->get();
+                    $this->EventUserSecurity = ( EventUserSecurity::where('type_event_report_id', $this->event_type_id)->exists())? EventUserSecurity::whereIn('dept_by_business_unit_id', [$value])->get():EventUserSecurity::whereIn('dept_by_business_unit_id', [$value])->where('type_event_report_id', $this->event_type_id)->get();
                     $this->show = true;
                 } else {
                     $this->show = false;
