02c6e8cd4fe993631ab2628235ad7538a04cf98c
yomangit <106853745+yomangit@users.noreply.github.com>
2025-02-06 11:47:41 +0800

    fix: remove debug statement in EventUserSecurity query logic in HazardReport component

--- app/Livewire/EventReport/HazardReport/Panal/Index.php
+++ app/Livewire/EventReport/HazardReport/Panal/Index.php
@@ -84,7 +84,7 @@ class Index extends Component
     {
         if ($this->procced_to === "ERM Assigned") {
             $ERM = ClassHierarchy::searchDivision(trim($this->division_id))->pluck('dept_by_business_unit_id');
-            dd($ERM);
+
             foreach ($ERM as $value) {
                 if (!empty($value)) {
                     $this->EventUserSecurity = ( EventUserSecurity::where('type_event_report_id', $this->event_type_id)->exists())? EventUserSecurity::whereIn('dept_by_business_unit_id', [$value])->get():EventUserSecurity::whereIn('dept_by_business_unit_id', [$value])->where('type_event_report_id', $this->event_type_id)->get();
