afa6983653ce447522ec74530277430a6e859a70
yomangit <106853745+yomangit@users.noreply.github.com>
2025-02-06 12:59:36 +0800

    fix: correct EventUserSecurity query logic in Index component

--- app/Livewire/EventReport/HazardReport/Panal/Index.php
+++ app/Livewire/EventReport/HazardReport/Panal/Index.php
@@ -86,7 +86,7 @@ class Index extends Component
             $ERM = ClassHierarchy::searchDivision(trim($this->division_id))->pluck('dept_by_business_unit_id');
             foreach ($ERM as $value) {
                 if (!empty($value)) {
-                    $this->EventUserSecurity = ( EventUserSecurity::where('responsible_role_id',2)->whereIn('dept_by_business_unit_id', [$value])->where('type_event_report_id', $this->event_type_id)->exists())? EventUserSecurity::whereIn('dept_by_business_unit_id', [$value])->where('type_event_report_id', $this->event_type_id)->get(): EventUserSecurity::whereIn('dept_by_business_unit_id', [$value])->get();
+                    $this->EventUserSecurity = ( EventUserSecurity::where('responsible_role_id',2)->where('dept_by_business_unit_id', $value)->where('type_event_report_id', $this->event_type_id)->exists())? EventUserSecurity::EventUserSecurity::where('responsible_role_id',2)->where('dept_by_business_unit_id', $value)->where('type_event_report_id', $this->event_type_id)->get(): EventUserSecurity::where('responsible_role_id',2)->where('dept_by_business_unit_id', $value)->get();
                     $this->show = true;
                 } else {
                     $this->show = false;
