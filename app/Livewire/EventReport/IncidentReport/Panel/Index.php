6e39c09c976762da842c3263a0f1e9a8e4609396
yomangit <106853745+yomangit@users.noreply.github.com>
2025-02-07 10:10:19 +0800

    fix: remove unused assignment of incident report fields in Index component

--- app/Livewire/EventReport/IncidentReport/Panel/Index.php
+++ app/Livewire/EventReport/IncidentReport/Panel/Index.php
@@ -27,8 +27,7 @@ class Index extends Component
     public function incident_updated()
     {
         $incidentReport = IncidentReport::whereId( $this->data_id)->first();
-        $this->assign_to = $incidentReport->assign_to;
-        $this->also_assign_to = $incidentReport->also_assign_to;
+
         $this->reference = $incidentReport->reference;
         $this->responsible_role_id = $incidentReport->WorkflowDetails->ResponsibleRole->id;
         $this->current_step = $incidentReport->WorkflowDetails->name;
