30c4939edcef2f1366e16e11c788be3636cd3710
yomangit <106853745+yomangit@users.noreply.github.com>
2025-03-25 16:04:46 +0800

    dsdsd

--- config/filesystems.php
+++ config/filesystems.php
@@ -38,7 +38,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('/storage'),
+            'root' => storage_path('storage/'),
             'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
