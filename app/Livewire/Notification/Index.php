41a47498974541006564d8fbae0f2962c636dccb
yomangit <106853745+yomangit@users.noreply.github.com>
2025-03-25 11:22:36 +0800

    asdfasdfasf

--- app/Livewire/Notification/Index.php
+++ app/Livewire/Notification/Index.php
@@ -3,49 +3,57 @@
 namespace App\Livewire\Notification;
 
 use Livewire\Component;
+
 class Index extends Component
 {
-    public $searching="",$hidden = false,$seleted_notif=[];
+    public $searching = "", $hidden = false, $seleted_notif = [];
     public function render()
     {
         $this->hidden = count($this->seleted_notif) <= 1;
         $allNotification  = auth()->user()->notifications;
         $unRead = auth()->user()->unreadNotifications;
-        return view('livewire.notification.index',[
-            'AllNotification' =>$allNotification,
-            'Unread'=>  $unRead
-            ]);
+        return view('livewire.notification.index', [
+            'AllNotification' => $allNotification,
+            'Unread' =>  $unRead
+        ]);
     }
-    public function readNotification($id,$url)
+    public function readNotification($id, $url)
     {
-        $notificationId = $id;
-        $userUnreadNotification = auth()->user()->unreadNotifications->where('id','like', $notificationId)->first();
-        if($userUnreadNotification) {
-            $userUnreadNotification->markAsRead();
+        if ($url) {
+            $notificationId = $id;
+            $userUnreadNotification = auth()->user()->unreadNotifications->where('id', 'like', $notificationId)->first();
+            if ($userUnreadNotification) {
+                $userUnreadNotification->markAsRead();
+                return redirect($url);
+            }
+        }
+    }
+    public function goTo($id, $url)
+    {
+        if ($url) {
+            $userUnreadNotification = auth()->user()->notifications->where('id', 'like', $id)->whereNull('read_at')->first();
+            if ($userUnreadNotification) {
+                $userUnreadNotification->markAsRead();
+                return redirect($url);
+            }
             return redirect($url);
         }
     }
-    public function goTo($id,$url){
-        $userUnreadNotification = auth()->user()->notifications->where('id','like', $id)->whereNull('read_at')->first();
-        if($userUnreadNotification) {
-            $userUnreadNotification->markAsRead();
-            return redirect($url);
-        }
-         return redirect($url);
-    }
-    public function delete($id){
-        $userDeleteNotification = auth()->user()->notifications->where('id','like', $id)->first();
-        if($userDeleteNotification) {
+    public function delete($id)
+    {
+        $userDeleteNotification = auth()->user()->notifications->where('id', 'like', $id)->first();
+        if ($userDeleteNotification) {
             $userDeleteNotification->delete();
         }
     }
-    public function deleteCheked(){
+    public function deleteCheked()
+    {
 
         $main =  auth()->user()->notifications->whereIn('id', $this->seleted_notif)->pluck('id');
 
         foreach ($main as $key => $value) {
-            $userDeleteNotification = auth()->user()->notifications->where('id','like', $value)->first();
-            if($userDeleteNotification) {
+            $userDeleteNotification = auth()->user()->notifications->where('id', 'like', $value)->first();
+            if ($userDeleteNotification) {
                 $userDeleteNotification->delete();
                 $this->seleted_notif = [];
             }
