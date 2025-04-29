92bc28446102266d7646cde75f224e7a71ee0fa6
yomangit <106853745+yomangit@users.noreply.github.com>
2025-04-29 09:43:25 +0800

    sdfsdf

--- app/Livewire/EventReport/HazardReport/CreateAndUpdate.php
+++ app/Livewire/EventReport/HazardReport/CreateAndUpdate.php
@@ -43,7 +43,7 @@ class CreateAndUpdate extends Component
 
     public function mount()
     {
-        $this->show_immidiate === '1';
+        $this->show_immidiate = 1;
         if (Auth::check()) {
             $reportBy = (Auth::user()->lookup_name) ? Auth::user()->lookup_name : Auth::user()->name;
             $this->report_byName = $reportBy;
