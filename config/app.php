0ac52fa886ab2dfc26dfcb3a07323381d52863b0
yomangit <106853745+yomangit@users.noreply.github.com>
2025-04-29 23:42:04 +0800

    dsdsd

--- config/app.php
+++ config/app.php
@@ -52,7 +52,8 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://tokasafe.archimining.com'),
+    'url' => env('APP_URL', 'http://localhost'),
+
 
     /*
     |--------------------------------------------------------------------------
